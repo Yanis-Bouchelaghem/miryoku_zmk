5c1e23cedbe2e2e9e896bf0c8de2d2defff9e283
Alphashow <lastblast@outlook.fr>
2025-10-05 16:33:44 +0200

    Customize num again

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -8,7 +8,7 @@ U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp 
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          \
-&kp LGUI,          &kp LALT,          &kp LSHFT,         &kp LCTRL,         U_NA,              &kp EQUAL,         &kp N4,            &kp N5,            &kp N6,            &kp SEMI,          \
-U_NA,              &kp RALT,          &u_to_U_NAV,       &u_to_U_NUM,       U_NA,              &kp BSLH,          &kp N1,            &kp N2,            &kp N3,            &kp GRAVE,         \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp MINUS,         &kp N0,            &kp DOT,           U_NP,              U_NP
+&kp EXCL,          &kp AT,            &kp LBRC,          &kp RBRC,          &kp PIPE,          &kp PLUS,          &kp N7,            &kp N8,            &kp N9,            &kp ASTERISK,      \
+U_MT(LGUI, HASH),  U_MT(LALT, DOLLAR),U_MT(LSHFT, LPAR), U_MT(LCTRL, RPAR), &kp GRAVE,         &kp MINUS,         &kp N4,            &kp N5,            &kp N6,            &kp COLON,         \
+&kp PERCENT,       &kp CARET,         &kp LBKT,          &kp RBKT,          &kp TILDE,         &kp EQUAL,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,         \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp N0,            &kp DOT,           U_NP,              U_NP
