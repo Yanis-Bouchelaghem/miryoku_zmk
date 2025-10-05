609941cf23e8c090e499d77957c7c88bb6f9dbef
Alphashow <lastblast@outlook.fr>
2025-10-05 17:15:47 +0200

    Customize nav layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -12,3 +12,9 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 U_MT(LGUI, HASH),  U_MT(LALT, DOLLAR),U_MT(LSHFT, LPAR), U_MT(LCTRL, RPAR), &kp GRAVE,         &kp MINUS,         &kp N4,            &kp N5,            &kp N6,            &kp COLON,         \
 &kp PERCENT,       &kp CARET,         &kp LBKT,          &kp RBKT,          &kp TILDE,         &kp EQUAL,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,         \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp N0,            &kp DOT,           U_NP,              U_NP
+
+#define MIRYOKU_LAYER_NAV \
+&kp F1,            &kp F2,            &kp F3,            &kp F4,            U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+U_MT(LGUI, F5),    U_MT(LALT, F6),    U_MT(LSHFT, F7),   U_MT(LCTRL, F8),   U_NA,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
+&kp F9,            &kp F10,           &kp F11,           &kp F12,           U_NA,              &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           &kp INS,           \
+U_NP,              U_NP,              &bt BT_CLR,        &bt BT_NXT,        U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
