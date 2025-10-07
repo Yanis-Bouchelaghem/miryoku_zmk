9886d88c48f8ea1eb34e2286e4c36ff7309d01e0
Alphashow <lastblast@outlook.fr>
2025-10-07 16:32:21 +0200

    Add back the nav button

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,7 +5,7 @@
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp P,             \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LSHFT, S),    U_MT(LCTRL, T),    &kp G,             &kp M,             U_MT(LCTRL, N),    U_MT(LSHFT, E),    U_MT(LALT, I),     U_MT(LGUI, O),\
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_NUM, ESC),  U_MT(LSHFT, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_MEDIA, BSPC), U_LT(U_FUN, DEL),U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_NUM, ESC),  U_MT(LSHFT, SPACE),U_LT(U_NAV, TAB),  U_LT(U_SYM, RET),  U_LT(U_MEDIA, BSPC), U_LT(U_FUN, DEL),U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
 &kp EXCL,          &kp AT,            &kp LBRC,          &kp RBRC,          &kp PIPE,          &kp PLUS,          &kp N7,            &kp N8,            &kp N9,            &kp ASTERISK,      \
@@ -17,4 +17,4 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 &kp F1,            &kp F2,            &kp F3,            &kp F4,            &bt BT_NXT,        U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_MT(LGUI, F5),    U_MT(LALT, F6),    U_MT(LSHFT, F7),   U_MT(LCTRL, F8),   &bt BT_PRV,        &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
 &kp F9,            &kp F10,           &kp F11,           &kp F12,           &bt BT_CLR,        &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           &kp INS,           \
-U_NP,              U_NP,              &u_out_tog,        U_NA,              &soft_off,         &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+U_NP,              U_NP,              &u_out_tog,        &soft_off,         U_NA,         &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
